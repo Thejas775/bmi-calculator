6894f129dd1a575c1d8c28345e8bedcf80957dc6
Thejas775 <thejaselandassery@gmail.com>
2023-11-02 13:21:48 +0530

    Removed extras

--- public/script.js
+++ public/script.js
@@ -2,19 +2,8 @@ function calculateBMI() {
     var name = document.getElementById("name").value;
     var height = parseFloat(document.getElementById("height").value);
     var weight = parseFloat(document.getElementById("weight").value);
-     //This is just to make fun of my friends
-    if (name.toLowerCase().includes("sanika")) {
-        var resultMessage = `Sanika better start going to the gym <br>Category: Hippo`;
-        document.getElementById("result").innerHTML = resultMessage;
-        return;
-    }
-    else if (name.toLowerCase().includes("aditi")) {
-        var resultMessage = `Aditi better start eating something <br>Category: Kuch kha le bhai`;
-        document.getElementById("result").innerHTML = resultMessage;
-        return;
-    }
-    //The above is just to make fun of my friends
-    else if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+   
+     if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
         document.getElementById("result").innerHTML = "Invalid input. Please enter valid height and weight.";
     } else {
         var bmi = weight / ((height / 100) ** 2);
